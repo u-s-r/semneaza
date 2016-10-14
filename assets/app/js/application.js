fb854d878dcd70d33f0b6f008e6f13a2c7fb2edb
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-10-14 16:18:03 +0300

    Updated deadline

--- assets/app/js/application.js
+++ assets/app/js/application.js
@@ -170,7 +170,7 @@ if ('undefined' === typeof window.USR) {
   }
 
   function step() {
-    var timespan = countdown(null, new Date(2016, 9, 16));
+    var timespan = countdown(null, new Date(2016, 9, 22));
 
     if (0 >= timespan.value) {
       $('#countdown-days').html('00');
