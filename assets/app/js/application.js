4d976bf2e4620ee4c652dcf32d108d00a664343b
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-10-17 21:46:33 +0300

    Target semnaturi

--- assets/app/js/application.js
+++ assets/app/js/application.js
@@ -8,7 +8,7 @@ if ('undefined' === typeof window.USR) {
   USR.data = {};
 
   USR.data.semnaturiStranse = 20659;
-  USR.data.targetSemnaturi = 200000;
+  USR.data.targetSemnaturi = 250000;
 
   USR.data.semnaturi = {
     'AB': 250,
