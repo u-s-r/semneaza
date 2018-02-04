e9d1b67f5231f3185eb004d13261a8045c559b12
ioana <peacelovetheysay@gmail.com>
2018-02-04 14:18:11 +0000

    header and input fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $data = get_data();
   </head>
   <body>
   <nav class="navbar navbar-default">
-    <div class="container-fluid">
+    <div class="container">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
           <span class="sr-only">Toggle navigation</span>
