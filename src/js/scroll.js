dfa3ee56a309687bb63ebc8bbeb9272041b63b9c
Andrei Zisu <matzipan@gmail.com>
2018-02-06 23:05:33 +0000

    Set hash after scroll

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,7 +1,14 @@
 $(document).on('click', 'a[href^="#"]', function (event) {
   event.preventDefault();
 
+  var $el = $(this);
+
   $('html, body').animate({
-    scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-  }, 500);
+    scrollTop: $('[name="' + $el.attr('href').substr(1) + '"]').offset().top
+  }, {
+    duration: 500,
+    complete: function () {
+        window.location.hash = $el.attr('href').substr(1);
+      }
+  });
 });
