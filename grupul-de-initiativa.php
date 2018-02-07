79444710b8560fa67fd784a687df79e399515616
Andrei Zisu <matzipan@gmail.com>
2018-02-07 19:59:06 +0000

    Pagina pentru grupul de initiativa

---
+++ grupul-de-initiativa.php
@@ -0,0 +1,103 @@
+<?php
+$page = "grupul-de-initiativa";
+
+require 'include/header.php';
+?>
+<div class="jumbotron jumbotron-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 banner">
+        <div>
+            <h1>Fără penali în funcții publice!</h1>
+        </div>
+        <h2>Inițiativa cetățenească de modificare a Constituției</h2>
+    </div>
+    </div>
+  </div>
+</div>
+<div class="container grupul-de-initiativa">
+    <h1>Grupul de inițiativă</h1>
+    <h4>Ajută la strângerea semnăturilor. Fiecare semnătură contează. Înscrie-te aici</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget tristique massa. Fusce dictum odio a rhoncus scelerisque. Proin rutrum lacus in nisi tempus placerat. Aliquam a cursus nisi, id mollis neque. Duis eu libero euismod, ullamcorper nibh aliquet, tempus erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc facilisis molestie rutrum. Duis dignissim, libero non varius mattis, nibh ante egestas tellus, quis semper metus enim eu ligula. Aliquam sed nunc sit amet nisl consectetur placerat.</p>
+    <p>Aliquam id ex ac augue lacinia mattis. Nam mollis non arcu sed malesuada. Donec tincidunt mi sed ante bibendum mattis. Vivamus bibendum convallis quam, in suscipit elit cursus et.</p>
+    <ul class="col-md-3 membri" role="tablist" aria-orientation="vertical">
+      <li><h3>Membri</h3></li>
+      <li class="active">
+        <a class="btn col-xs-6 col-sm-6 col-md-12" id="mihai-vasile-tab" href="#mihai-vasile" role="pill" data-toggle="pill" aria-controls="media-tabs-video" aria-selected="true">
+          <img src="build/img/grupul-de-initiativa/some-dude.png" alt="">
+          Mihai Vasile
+          <span class="glyphicon glyphicon-menu-right"></span>
+        </a>
+      </li>
+      <li>
+        <a class="btn col-xs-6 col-sm-6 col-md-12" id="georgeta-ionescu-tab" href="#georgeta-ionescu" role="pill" data-toggle="pill" aria-controls="media-tabs-photo" aria-selected="false">
+          <img src="build/img/grupul-de-initiativa/some-lady.png" alt="">
+          Georgeta Ionescu
+          <span class="glyphicon glyphicon-menu-right"></span>
+        </a>
+      </li>
+    </ul>
+    <div class="col-md-9">
+      <div class="tab-content">
+        <div class="tab-pane fade active in" id="mihai-vasile" role="tabpanel" aria-labelledby="mihai-vasile-tab">
+            <h3>Despre</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque ligula vitae tempus maximus. Cras tempor maximus arcu. Mauris molestie ligula non blandit varius. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Interdum et malesuada fames ac ante ipsum primis in faucibus. In semper consectetur turpis, sed laoreet mauris fermentum at. Donec sollicitudin urna faucibus, placerat mi sit amet, pulvinar urna. Curabitur et enim ante. Etiam bibendum leo dolor, efficitur molestie ex pulvinar ut. Maecenas pulvinar viverra justo sed finibus. Aenean ac euismod lacus. Maecenas et luctus velit, ac cursus dolor.
+            </p>
+            <p>
+                Ut risus diam, faucibus a ipsum vitae, tempus iaculis orci. Phasellus tristique tempus euismod. Curabitur et convallis ipsum. Aenean lobortis orci magna, eget porttitor dolor malesuada quis. Donec scelerisque laoreet nulla. Donec sed ornare felis. Cras accumsan sem nec pretium maximus. Cras sit amet elit eros. In et mi quis tellus auctor tristique. Nunc vitae libero a purus faucibus mollis posuere sed leo. Morbi at dolor accumsan, gravida mauris sed, faucibus est. Vivamus molestie est eget felis pellentesque interdum.
+                Etiam bibendum leo dolor, efficitur molestie ex pulvinar ut. Maecenas pulvinar viverra justo sed finibus. Aenean ac euismod lacus. Maecenas et luctus velit, ac cursus dolor.
+            </p>
+            <p>
+                Nam quis tempor dolor. Aliquam ac finibus nibh. Pellentesque at porttitor velit. Fusce tincidunt sapien nisi, ac fermentum ex hendrerit vel. Fusce vehicula velit vitae nibh congue cursus. Praesent congue tellus eu mi consectetur, vitae tincidunt orci tincidunt. Mauris diam lacus, bibendum id augue id, laoreet tincidunt felis.
+            </p>
+        </div>
+        <div class="tab-pane fade" id="georgeta-ionescu" role="tabpanel" aria-labelledby="georgeta-ionescu-tab">
+            <h3>Despre</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque ligula vitae tempus maximus. Cras tempor maximus arcu. Mauris molestie ligula non blandit varius. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Interdum et malesuada fames ac ante ipsum primis in faucibus. In semper consectetur turpis, sed laoreet mauris fermentum at. Donec sollicitudin urna faucibus, placerat mi sit amet, pulvinar urna. Curabitur et enim ante. Etiam bibendum leo dolor, efficitur molestie ex pulvinar ut. Maecenas pulvinar viverra justo sed finibus. Aenean ac euismod lacus. Maecenas et luctus velit, ac cursus dolor.
+            </p>
+            <p>
+                Ut risus diam, faucibus a ipsum vitae, tempus iaculis orci. Phasellus tristique tempus euismod. Curabitur et convallis ipsum. Aenean lobortis orci magna, eget porttitor dolor malesuada quis. Donec scelerisque laoreet nulla. Donec sed ornare felis. Cras accumsan sem nec pretium maximus. Cras sit amet elit eros. In et mi quis tellus auctor tristique. Nunc vitae libero a purus faucibus mollis posuere sed leo. Morbi at dolor accumsan, gravida mauris sed, faucibus est. Vivamus molestie est eget felis pellentesque interdum.
+                Etiam bibendum leo dolor, efficitur molestie ex pulvinar ut. Maecenas pulvinar viverra justo sed finibus. Aenean ac euismod lacus. Maecenas et luctus velit, ac cursus dolor.
+            </p>
+            <p>
+                Nam quis tempor dolor. Aliquam ac finibus nibh. Pellentesque at porttitor velit. Fusce tincidunt sapien nisi, ac fermentum ex hendrerit vel. Fusce vehicula velit vitae nibh congue cursus. Praesent congue tellus eu mi consectetur, vitae tincidunt orci tincidunt. Mauris diam lacus, bibendum id augue id, laoreet tincidunt felis.
+            </p>
+        </div>
+      </div>
+
+    </div>
+</div>
+<div class="formular">
+    <div class="container">
+        <form id="date-contact" method="post" action="date-contact.php">
+            <a name="formular"></a>
+            <h1>Alătură-te inițiativei</h1>
+            <div class="row">
+                <p class="col-md-9">Încă nu am început strângerea de semnături, <strong>inițiativa așteaptă avizul legislativ</strong>. Lasă-ne aici datele și te vom contacta noi când începem să strângem semnăturile. </p>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                  <h4>Numele și prenume</h4>
+                  <input type="text" name="nume">
+                </div>
+                <div class="col-md-4">
+                  <h4>Telefon</h4>
+                  <input type="text" name="telefon">
+                </div>
+                <div class="col-md-4">
+                  <h4>Email</h4>
+                  <input type="text" name="email">
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-default" onclick="ga('send', 'event', 'pagina_semnaturi', 'buton', 'trimite_date_contact')">
+              Înscrie-te acum!
+            </button>
+        </form>
+    </div>
+</div>
+<?php
+require_once 'include/footer.php';
+?>
